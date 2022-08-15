8c2670f2126fc5e4845701455f46760f920e022c
bruno_amoedo <brunoamoedo10@hotmail.com>
2022-08-15 18:54:36 +0000

    configuração do servidor web

---
+++ server_web.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+apt-get update
+apt-get upgrade -y
+
+echo "Instalando apache e unzip"
+apt-get install apache2 -y
+apt-get install unzip -y
+cd /tmp
+echo "Baixando os arquivos da aplicação"
+wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
+unzip main.zip
+cd linux-site-dio
+cp -R * /var/www/html/
+
